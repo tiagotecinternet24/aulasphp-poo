ff8c297167f5497ffe069b3051c317929e79068b
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-08 11:04:52 -0300

    Cria index básica

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01</title>
+</head>
+<body>
+    <h1>PHP com POO - Exemplo 01</h1>
+    <hr>
+    
+</body>
+</html>
