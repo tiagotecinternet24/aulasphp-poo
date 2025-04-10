a8ea05310ce40ad7bd83d1a81b0da881aae7b630
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-10 09:54:36 -0300

    Altera para exemplo 02

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 01</title>
+    <title>Exemplo 02</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 01</h1>
+    <h1>PHP com POO - Exemplo 02</h1>
     <hr>
 
 <?php
