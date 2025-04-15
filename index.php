73e1d7740a899c1b47a10110cd6459355919fceb
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-15 11:37:10 -0300

    Aplica herança

--- index.php
+++ index.php
@@ -10,9 +10,13 @@
     <hr>
 
 <?php
+require_once "src/Cliente.php"; // Superclasse
+require_once "src/PessoaFisica.php"; // Subclasse
 
+$clientePF = new PessoaFisica("Tiago", "tiago@gmail.com");
 ?>
 
+<pre><?=var_dump($clientePF)?></pre>
 
 
 </body>
---
+++ src/PessoaFisica.php
@@ -0,0 +1,6 @@
+<?php
+class PessoaFisica extends Cliente
+{
+    private int $idade;
+    private string $cpf;
+}
