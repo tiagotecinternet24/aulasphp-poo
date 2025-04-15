c39e19c28d63ac68825e0d5bb0599e6599b3ba78
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-15 09:38:29 -0300

    Refatora condicional

--- src/Cliente.php
+++ src/Cliente.php
@@ -14,15 +14,10 @@ class Cliente
     }
 
     public function verificarIdade(): string {
-        if( $this->idade < 18 ){
-            return "menor de idade";
-        } elseif($this->idade < 60) {
-            return "adulto";
-        } else {
-            return "idoso";
-        }
+        if( $this->idade < 18 ) return "menor de idade";
+        if( $this->idade < 60 ) return "adulto";
+        return "idoso";
     }
-
 
     public function setNome(string $nome): void 
     {
