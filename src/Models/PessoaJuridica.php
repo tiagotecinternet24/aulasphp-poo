1808567ea9e264574a78a3066e8965563a5c1a00
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-17 12:01:18 -0300

    Aplica método com sobreescrita

--- src/Models/PessoaJuridica.php
+++ src/Models/PessoaJuridica.php
@@ -18,6 +18,19 @@ class PessoaJuridica extends Cliente {
         $this->setSituacao(Situacao::PENDENTE);
     }
 
+    /* Método polimórfico, ou seja, ele sobreescreve 
+    o método originalmente criado na Superclasse.
+    Atenção: os métodos devem ter a mesma assinatura (nome, parâmetros e retorno) */
+    public function relatorio(): string
+    {
+        return "<div>"
+            . parent::relatorio() . // opcional
+            "<p><b>Nome Fantasia:</b> {$this->getNomeFantasia()} </p>
+            <p><b>CNPJ:</b> {$this->getCnpj()} </p>
+        </div>";
+    }
+
+
     private function setCnpj(string $cnpj): void {
         $this->cnpj = $cnpj;
     }
