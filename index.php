d4879d854ddfc39f75cb03eeead2b5f7f4a3a565
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-17 11:30:13 -0300

    Reorganiza pastas em src

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <hr>
 
 <?php
-require_once "src/Situacao.php"; // Enum
-require_once "src/Cliente.php"; // Superclasse
-require_once "src/PessoaFisica.php"; // Subclasse
-require_once "src/PessoaJuridica.php"; // Subclasse
+require_once "src/Enums/Situacao.php"; // Enum
+require_once "src/Models/Cliente.php"; // Superclasse
+require_once "src/Models/PessoaFisica.php"; // Subclasse
+require_once "src/Models/PessoaJuridica.php"; // Subclasse
 
 $clientePF = new PessoaFisica("Tiago", "tiago@gmail.com", 30, "123.456.789-00");
 $clientePJ = new PessoaJuridica(
