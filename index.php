810f7ae97d16d6368f7bbd57df679174f894fd6b
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-29 08:54:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,16 +19,7 @@ $clientePF = new PessoaFisica("Tiago", "tiago@gmail.com", 30, "123.456.789-00");
 $clientePJ = new PessoaJuridica(
     "João Pedro", "joao@tabajara.com", "32.789.5555/000-1", 2019, "Corporação Tabajara");
 
-/* Código abaixo dará erro, pois Cliente é uma classe abstrata, 
-portanto, não podemos criar objeto a partir dela. */
-// $clienteGenerico = new Cliente("Fulano", "fulano@gmail.com");
-// var_dump($clienteGenerico);
-
-/* O código abaixo dará erro, pois não é possível
-definir/usar uma subclasse que extenda PessoaJuridica (que é uma classe final) */
-//require_once "src/Models/MEI.php";
-//$clienteMEI = new MEI("AAA", "a@a.com", "123", 2025, "aaaaAA");
-//var_dump($clienteMEI);
+    
 ?>
     <h2>Relatórios</h2>
     
